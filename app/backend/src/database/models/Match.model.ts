e9d12a7093a282b43c6c19994668f7606d3a107c
Jayromberg <jayro.dev@gmail.com>
2022-10-25 17:22:03 -0300

    feat: Desenvolvida a model de partidas (matches)

---
+++ app/backend/src/database/models/Match.model.ts
@@ -0,0 +1,68 @@
+import { BOOLEAN, INTEGER, Model } from 'sequelize';
+import db from '.';
+import User from './User.model';
+
+class Match extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'home_team',
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'away_team',
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+    field: 'in_progress',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Match.belongsTo(User, { foreignKey: 'homeTeam', as: 'teamHome' });
+Match.belongsTo(User, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Match;
