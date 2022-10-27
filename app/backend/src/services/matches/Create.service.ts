77f77a279b7f87d4996de34f564fa859ed2ed090
Jayromberg <jayro.dev@gmail.com>
2022-10-27 18:27:29 -0300

    feat: Implementada a verificação do time antes da criação da partida

--- app/backend/src/services/matches/Create.service.ts
+++ app/backend/src/services/matches/Create.service.ts
@@ -1,4 +1,7 @@
 import Match from '../../entities/matches/Match.entity';
+import SequelizeFindOneTeamRepository
+  from '../../repositories/teams/SequelizeFindOneTeam.repository';
+import HttpException from '../../utils/http.exception';
 import { IMatch } from '../../interfaces/match';
 
 export interface CreateMatchRepository {
@@ -7,9 +10,11 @@ export interface CreateMatchRepository {
 
 export default class CreateMatchService {
   private _repository;
+  private _team: SequelizeFindOneTeamRepository;
 
   constructor(repository: CreateMatchRepository) {
     this._repository = repository;
+    this._team = new SequelizeFindOneTeamRepository();
   }
 
   async create(
@@ -19,7 +24,14 @@ export default class CreateMatchService {
     awayTeamGoals: number,
   ) {
     const match = new Match(homeTeam, awayTeam, homeTeamGoals, awayTeamGoals);
+    await this.teamExist(homeTeam);
+    await this.teamExist(awayTeam);
     const data = await this._repository.create(match);
     return data;
+  }
+
+  private async teamExist(id: number) {
+    const team = await this._team.findOne(id);
+    if (!team) throw new HttpException(404, 'There is no team with such id!');
   }
 }
