083cf9c25ee0e6a5bf1400ab313a42db74eef1b5
Jayromberg <jayro.dev@gmail.com>
2022-10-27 15:31:56 -0300

    test: Desenvolvido o teste de sucesso na rota /matches/:id/finish

---
+++ app/backend/src/tests/matches.update.test.ts
@@ -0,0 +1,60 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/User.model';
+import Match from '../database/models/Match.model';
+
+import { Response } from 'superagent';
+import { any } from 'joi';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teste de integração da rola POST /matches', () => {
+  let chaiHttpResponse: Response;
+  let token: string;
+
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({
+        id: 1,
+        username: 'Admin',
+        role: 'admin',
+        email: 'admin@admin.com',
+        password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+      } as User);
+
+    sinon
+      .stub(Match, "update")
+      .resolves();
+
+    chaiHttpResponse = await chai.request(app)
+      .post('/login')
+      .send({
+        email: 'admin@admin.com',
+        password: 'secret_admin'
+      });
+
+    token = chaiHttpResponse.body.token
+  });
+
+  after(async () => {
+    (User.findOne as sinon.SinonStub).restore();
+    (Match.update as sinon.SinonStub).restore();
+  })
+
+  it('Returno da rota PUT /matches em caso de sucesso', async () => {
+    chaiHttpResponse = await chai.request(app)
+      .put('/matches/1/finish')
+      .set('Authorization', token)
+
+    expect(chaiHttpResponse.status).to.equal(200);
+    expect(chaiHttpResponse.body).to.deep.equal({ message: "Finished" });
+  })
+});
