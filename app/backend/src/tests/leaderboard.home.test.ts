3588bc06607f5bcd59185819c05f8ddfe079ef02
Jayromberg <jayro.dev@gmail.com>
2022-10-29 12:29:22 -0300

    test: Desenvolvido o teste para o caso de sucesso da rota /leaderboard/home

---
+++ app/backend/src/tests/leaderboard.home.test.ts
@@ -0,0 +1,154 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/User.model';
+import Match from '../database/models/Match.model';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teste de integração da rola /leaderboard/home', () => {
+  let chaiHttpResponse: Response;
+  let token: string;
+
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({
+        id: 1,
+        username: 'Admin',
+        role: 'admin',
+        email: 'admin@admin.com',
+        password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+      } as User);
+
+    sinon
+      .stub(Match, "findAll")
+      .resolves([
+          {
+            id: 1,
+            homeTeam: 16,
+            homeTeamGoals: 1,
+            awayTeam: 8,
+            awayTeamGoals: 1,
+            inProgress: false,
+            teamHome: {
+              teamName: "São Paulo"
+            },
+            teamAway: {
+              teamName: "Grêmio"
+            }
+          },
+          {
+            id: 2,
+            homeTeam: 9,
+            homeTeamGoals: 2,
+            awayTeam: 14,
+            awayTeamGoals: 0,
+            inProgress: true,
+            teamHome: {
+              teamName: "São Paulo"
+            },
+            teamAway: {
+              teamName: "Internacional"
+            }
+          },
+          {
+            id: 3,
+            homeTeam: 4,
+            homeTeamGoals: 3,
+            awayTeam: 11,
+            awayTeamGoals: 0,
+            inProgress: false,
+            teamHome: {
+              teamName: "Corinthians"
+            },
+            teamAway: {
+              teamName: "Napoli-SC"
+            }
+          },
+          {
+            id: 4,
+            homeTeam: 3,
+            homeTeamGoals: 0,
+            awayTeam: 2,
+            awayTeamGoals: 0,
+            inProgress: false,
+            teamHome: {
+              teamName: "Botafogo"
+            },
+            teamAway: {
+              teamName: "Bahia"
+            }
+          },
+        ] as unknown as Match[]);
+
+    chaiHttpResponse = await chai.request(app)
+      .post('/login')
+      .send({
+        email: 'admin@admin.com',
+        password: 'secret_admin'
+      });
+
+    token = chaiHttpResponse.body.token
+  });
+
+  after(async () => {
+    (User.findOne as sinon.SinonStub).restore();
+    (Match.findAll as sinon.SinonStub).restore();
+  })
+
+  it('Returno da rota /leaderboard/home em caso de sucesso', async () => {
+    chaiHttpResponse = await chai.request(app)
+    .get('/leaderboard/home')
+    .set('Authorization', token)
+
+    expect(chaiHttpResponse.status).to.equal(200);    
+    expect(chaiHttpResponse.body).to.deep.equal(
+      [
+        {
+          name: 'Corinthians',
+          totalPoints: 3,
+          totalGames: 1,
+          totalVictories: 1,
+          totalDraws: 0,
+          totalLosses: 0,
+          goalsFavor: 3,
+          goalsOwn: 0,
+          goalsBalance: 3,
+          efficiency: '100.00'
+        },
+        {
+          name: 'São Paulo',
+          totalPoints: 1,
+          totalGames: 1,
+          totalVictories: 0,
+          totalDraws: 1,
+          totalLosses: 0,
+          goalsFavor: 1,
+          goalsOwn: 1,
+          goalsBalance: 0,
+          efficiency: '33.33'
+        },
+        {
+          name: 'Botafogo',
+          totalPoints: 1,
+          totalGames: 1,
+          totalVictories: 0,
+          totalDraws: 1,
+          totalLosses: 0,
+          goalsFavor: 0,
+          goalsOwn: 0,
+          goalsBalance: 0,
+          efficiency: '33.33'
+        }
+      ]);
+  })
+});
